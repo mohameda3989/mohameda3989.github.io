53d87901e5602b467bd66b50323495fe4405081a
mohameda3989 <74214804+mohameda3989@users.noreply.github.com>
2020-11-09 17:04:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Mohamed Alzawoqari"
+let userName = "mohameda3989";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
